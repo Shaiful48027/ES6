e7afce83fbcd4a4ee10cef694a01480b5b081d8b
Shaiful <shaiful48027@gmail.com>
2024-12-14 10:43:42 +0600

    array.map

---
+++ export.js
@@ -0,0 +1,19 @@
+const product = ['mouse', 'keybord', 'monitor'];
+const user = {
+    name : "shaiful",
+    age : 30,
+    sub : "bangla",
+    id : 456
+};
+
+
+
+function sum (a, b){
+    console.log(a + b); 
+};
+
+export default {
+    product,
+    user,
+    sum
+}

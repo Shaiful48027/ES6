863ee82ef0f5b66f6e0263e5adc49372eb7294bd
Shaiful <shaiful48027@gmail.com>
2024-12-14 10:48:33 +0600

    concat.js

---
+++ concat.js
@@ -0,0 +1,16 @@
+let num1 = [1, 2, 3];
+let num2 = [10, 22, 33];
+
+
+let result =  num1.concat(num2);
+console.log(result);
+
+
+let names = ['shaiful', 'naim', 'rufayda'];
+let name2 = ['siam', 'sakib', 'rafi'];
+
+
+let results = names.concat(name2);
+console.log(results);
+
+
--- import.js
+++
@@ -1,6 +0,0 @@
-let number = [1,2, 3, 5, 6, 8, 9];
-
-let result = number.map((num)=>{
-    return 2*num;
-});
-console.log(result);
